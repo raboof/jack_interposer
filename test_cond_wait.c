8309bfd9fce6a102d299698b191d335889e2b24d
Arnout Engelen <arnouten@localhost.(none)>
2010-01-17 16:42:58 +0100

    added test_cond_wait, made test code more reusable

--- test_cond_wait.c
+++ test_cond_wait.c
@@ -1,30 +1,12 @@
-#include <jack/jack.h>
+#include "test_framework.c"
 #include <pthread.h>
-#include <stdbool.h>
-#include <stdio.h>
 
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-bool been_in_process = false;
 
 int test_process_callback(jack_nframes_t nframes, void* arg)
 {
   pthread_cond_wait(&cond, &mutex);
   been_in_process = true;
   return 1;
-}
-
-int main()
-{
-  jack_client_t * client = jack_client_open("testclient", JackNullOption, NULL);
-  jack_set_process_callback(client, test_process_callback, NULL);
-  jack_activate(client);
-  while(!been_in_process)
-  {
-    pthread_cond_signal(&cond);
-  }
-  jack_deactivate(client);
-  printf("Been in process: %d\n", been_in_process);
-  return 0;
 }
--- test_new.cpp
+++ test_new.cpp
@@ -1,11 +1,4 @@
-#include <jack/jack.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-
-bool been_in_process = false;
+#include "test_framework.c"
 
 class Object 
 {
@@ -15,23 +8,4 @@ int test_process_callback(jack_nframes_t nframes, void* arg)
 {
   new Object();
   return 1;
-}
-
-void handle_abort(int sig)
-{
-  // we should abort
-  exit(0);
-}
-
-int main()
-{
-  signal(SIGABRT, handle_abort);
-
-  jack_client_t * client = jack_client_open("testclient", JackNullOption, NULL);
-  jack_set_process_callback(client, test_process_callback, NULL);
-  jack_activate(client);
-  sleep(1);
-  jack_deactivate(client);
-  printf("Been in process: %d\n", been_in_process);
-  return -1;
 }
